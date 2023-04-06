297675b47de7030fb74051ed6669b08d6f42e36d
JbCantel <joaobcantel10@gmail.com>
2023-04-05 21:33:30 -0300

    SiteScript

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 $(document).ready(myApp)
 
+function myApp() {
 
-function myApp (){
+    // Carrega a página inicial.
+    loadpage('home')
 
-console.log('funciona!')
+}
+
+// Carrega uma pagina.
+/* O caminho final da página é passado como parâmetro da função. */
+function loadpage(page) {
+
+    // Monta os caminhos para os componentes da página solicitada.
+    const path = {
+        html: `/pages/${page}/index.html`,
+        css: `pages/${page}/index.css`,
+        js: `pages/${page}/index.js`
+    }
+
+    $.get(path.html)
+        .done((data) => {
+            $('main').html(data)
+        })
+        .fail((error) => {
+            loadpage('e404')
+        })
 
 }
