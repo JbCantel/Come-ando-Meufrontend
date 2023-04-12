5e00ada2a12ec2b3e3aa7041eddfb4b91c7de6a8
JbCantel <joaobcantel10@gmail.com>
2023-04-11 21:45:31 -0300

    formulário de contatos

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -9,4 +9,28 @@ function myContacts(){
      * Altera o título da página contacts
      */
     changeTitle ('Faça contato')
+
+    $(document).on('submit', '#cForm', sendContact)
+
+}
+
+function sendContact(ev) {
+
+    var formJSON = {}
+
+    const formData = new FormData(ev.target);
+
+    for (const [key, value] of formData) {
+        formJSON[key] = value
+    }
+    
+    // Gera o JSON a ser enviado para a API.
+    formJSON = JSON.stringify(formJSON)
+
+    console.log(formJSON)
+    
+    // console.log(JSON.parse(formJSON))
+
+ return false
+
 }
